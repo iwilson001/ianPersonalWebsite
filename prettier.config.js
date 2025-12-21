d7067e2fcdb1295f25688551288d6847c9769035
ian <ianw2000ct@gmail.com>
2025-12-20 19:33:59 -0600

    Updates prettier config

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    printWidth: 120,
+    trailingComma: 'all',
+    tabWidth: 4,
+    arrowParens: 'always',
+    semi: true,
+    singleQuote: true,
+    bracketSameLine: false,
+    bracketSpacing: false,
+    jsxSingleQuote: true,
+    useTabs: false,
+};
+
+export default config;
